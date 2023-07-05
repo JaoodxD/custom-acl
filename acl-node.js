1029597b6fb95b1d0b2691a1c77ffd72e3db9e72
JaoodxD <jaood96@gmail.com>
2023-07-05 15:01:58 +0300

    add primitive tree node data structure to store configs

---
+++ acl-node.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const validator = {
+  get(target, key) {
+    if (typeof target[key] === 'object' && target[key] !== null) {
+      return new Proxy(target[key], validator);
+    } else {
+      return target[key];
+    }
+  }
+};
+
+
+class ACLNode {
+  #name;
+  #config;
+  #children;
+  constructor(config, name = 'anonymous node') {
+    this.#config = config;
+    this.#name = name;
+  }
+
+  update(newConfig) {
+    this.#config = newConfig;
+    console.log('Config updated in ', this.#name);
+    this.updateChilderen();
+  }
+
+  get config() {
+    return this.config;
+  }
+
+  set config(value) {
+    this.#children = value;
+    this.updateChilderen();
+  }
+
+  updateChilderen() {
+    for (const child of this.#children) {
+      if (!child) continue;
+      if (!child.update) continue;
+      child.update(this.#config);
+    }
+  }
+}
