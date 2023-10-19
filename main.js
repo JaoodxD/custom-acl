e846a79b484d615a53e6c26ee7c1719d34837ee8
JaoodxD <jaood96@gmail.com>
2023-10-19 14:56:00 +0300

    Add all kind of exports to `main.js`

--- main.js
+++ main.js
@@ -2,45 +2,6 @@
 
 const AclNode = require('./lib/acl-node.js');
 
-
-// const head = new ACLNode({ config: {}, name: 'global config' });
-// const group = new ACLNode({ config: {}, name: 'group: programmers' });
-// const user = new ACLNode({ config: {}, name: 'user: D1mASS14ek' });
-
-// head.appendChildren(group);
-// group.appendChildren(user);
-
-// const printConfigs = () => console.table({
-//   head: JSON.stringify(head.config.countries),
-//   group: JSON.stringify(group.config.countries),
-//   user: JSON.stringify(user.config.countries)
-// });
-
-// printConfigs();
-// head.update({
-//   countries: {
-//     UA: true,
-//     KZ: false,
-//     TR: false,
-//     time: 1
-//   }
-// });
-// printConfigs();
-// head.update({
-//   countries: {
-//     KZ: true,
-//   }
-// });
-// printConfigs();
-// group.update({
-//   countries: {
-//     TR: true,
-//   }
-// });
-// printConfigs();
-// head.update({
-//   countries: {
-//     UA: false,
-//   }
-// });
-// printConfigs();
+module.exports = AclNode;
+module.exports.default = AclNode;
+module.exports.AclNode = AclNode;
