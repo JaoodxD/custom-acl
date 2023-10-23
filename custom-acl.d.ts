255bd1b6deca5ea1be5b164c9fbcf4a94b8d5612
JaoodxD <jaood96@gmail.com>
2023-10-23 16:49:15 +0300

    Update types

--- custom-acl.d.ts
+++ custom-acl.d.ts
@@ -2,7 +2,7 @@ export declare class AclNode {
   name: string;
   parent: AclNode | null;
   private #config: Record<string, any>;
-  private #colorDot: any;
+  private #colorDot?: RELATION_TYPE;
   private #children: AclNode[];
   private #merge: ((config: Record<string, any>, newConfig: Record<string, any>) => void) | null;
   private #diff: ((config: Record<string, any>, newConfig: Record<string, any>) => Record<string, any>) | null;
@@ -20,10 +20,20 @@ export declare class AclNode {
   appendChildren(node: AclNode): void;
   removeChild(node: AclNode): void;
   update(newConfig: Record<string, any>): void;
+  get dotColor(): RELATION_TYPE;
+  recalcDot();
+  calcRelation(): RELATION_TYPE;
   recalcColorDot(): void;
   get config(): Record<string, any>;
   set config(value: Record<string, any>);
   updateChildren(cfg?: Record<string, any>): void;
+}
+
+declare enum RELATION_TYPE {
+  SUBSET = 'red',
+  SUPERSET = 'blue',
+  OVERLAPPING = 'red-blue',
+  EQUAL = '',
 }
 
 export default AclNode
